ce8c56d3843e3591c0c4c7af756a06acd533c652
TyliOnel <tylionel95@gmail.com>
2024-04-18 21:42:53 -0400

    code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 import sqlalchemy
 from sqlalchemy import text
+from flask import render_template
 
 app = flask.Flask(__name__)
 
@@ -16,14 +17,13 @@ def get_data():
     result = conn.execute(text(query))
     # Fetch the data
     data = result.fetchall()
-    print(data)
     # Close the connection
     conn.close()
     return data
 
-# @app.route('/')
-# def main():
-#     return flask.render_template('index.html')
+@app.route('/')
+def home():
+    return flask.render_template("smmh.html")
 
 @app.route('/social')
 def get_social():
@@ -41,4 +41,39 @@ def get_social():
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True)
-    
+
+
+# import flask
+# import sqlalchemy
+# from sqlalchemy import text
+# from flask import render_template, jsonify
+
+# app = flask.Flask(__name__)
+
+# def get_data():
+#     # Create a connection to the database
+#     connection_string = 'postgresql://postgres:postgres@localhost:5432/proj3_db'
+#     # Create the engine
+#     engine = sqlalchemy.create_engine(connection_string)
+#     # Establish a connection
+#     conn = engine.connect()
+#     # Execute a query to fetch data from the database
+#     query = "SELECT * FROM smmh_data"
+#     result = conn.execute(text(query))
+#     # Fetch the data
+#     data = result.fetchall()
+#     # Close the connection
+#     conn.close()
+#     return data
+
+# @app.route('/')
+# def home():
+#     return flask.render_template("smmh.html")
+
+# @app.route('/social')
+# def get_social():
+#     data = get_data()
+#     return flask.jsonify(data)
+
+# if __name__ == "__main__":
+#     app.run(host='localhost', debug=True)
