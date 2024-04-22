c94ce755bc0584efcae6ff9001f0d44fc879a819
TyliOnel <tylionel95@gmail.com>
2024-04-22 18:52:19 -0400

    visualization

--- app.py
+++ app.py
@@ -1,43 +1,55 @@
 import flask
 import sqlalchemy
 from sqlalchemy import text
-from flask import render_template
+from flask import render_template, jsonify
 
 app = flask.Flask(__name__)
-
-def get_data():
-    # Create a connection to the database
+def get_data(query):
     connection_string = 'postgresql://postgres:postgres@localhost:5432/proj3_db'
     # Create the engine
     engine = sqlalchemy.create_engine(connection_string)
     # Establish a connection
     conn = engine.connect()
-    # Execute a query to fetch data from the database
-    query = "SELECT * FROM smmh_data"
     result = conn.execute(text(query))
     # Fetch the data
     data = result.fetchall()
-    # Close the connection
     conn.close()
     return data
-
 @app.route('/')
-def home():
-    return flask.render_template("smmh.html")
-
-@app.route('/social')
-def get_social():
-    data = get_data()
-    cols = ["Age", "Gender", "RelationshipStatus", "Occupation", "Organization", "SocialMedia", "SocialMediaPlatforms", "NumberofSocialMediaPlatforms", "HoursSpent", "Frequency", "Distraction", "Restlessness", "Anxiety", "ConcentrationDifficulty", "SelfComparision", "PostSentiment", "ValidationSeeking", "Depression", "ActivityInterest", "Sleeplessness", "Reddit", "YouTube", "Snapchat", "Pinterest", "TikTok", "Instagram", "Discord", "Facebook", "Twitter", "two_to_five_hrs", "less_than_two_hrs", "more_than_five_hr"]
+def main():
+    return flask.render_template('smmh.html')
+@app.route('/data')
+def get_social_data():
+    data = get_data("SELECT * FROM smmh_data")
     all_data = []
     for record in data:
-        data_json = {}
-        # for i, col in enumerate(cols):
-        #     data_json[col] = record[i]
-        #     all_data.append(data_json)
         all_data.append(dict(record._mapping))
-    
     return flask.jsonify(all_data)
+
+@app.route('/age_select_data')
+def get_age_select():
+    query = """
+    SELECT
+        age,
+        SUM(CAST(reddit AS INTEGER)) AS reddit,
+        SUM(CAST(youtube AS INTEGER)) AS youtube,
+        SUM(CAST(snapchat AS INTEGER)) AS snapchat,
+        SUM(CAST(pinterest AS INTEGER)) AS pinterest,
+        SUM(CAST(tiktok AS INTEGER)) AS tiktok,
+        SUM(CAST(instagram AS INTEGER)) AS instagram,
+        SUM(CAST(discord AS INTEGER)) AS discord,
+        SUM(CAST(facebook AS INTEGER)) AS facebook,
+        SUM(CAST(twitter AS INTEGER)) AS twitter
+    FROM smmh_data
+    GROUP BY age
+    ORDER BY age;
+    """
+    data = get_data(query)
+    platform_data = []
+    for record in data:
+        platform_data.append(dict(record._mapping))
+    # platform_data = [dict(record) for record in data]
+    return jsonify(platform_data)
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True)
